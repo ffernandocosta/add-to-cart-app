7c3e56558f54c770124ca3d3e201429333aac667
Fernando Costa <ferndcosta353@gmail.com>
2023-07-15 08:26:53 -0300

    fix: correcting bug when deleting last item
    The error occurred because when deleting the last item it also deletes the reference. So when the 'onValue' method runs it doesn't return a snapshot because it couldn't find the reference 'shoppingListInDB'.

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,16 +20,21 @@ addButtonEl.addEventListener('click', () => {
 });
 
 onValue(shoppingListInDB, (snapshot) => {
-    let itemsArray = Object.entries(snapshot.val());
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val());
 
-    clearShoppingListEl
-    
-    for (let i = 0; i < itemsArray.length; i++) {
-        let currentItem = itemsArray[i];
-        let currentItemID = currentItem[0];
-        let currentItemValue = currentItem[1];
+        clearShoppingListEl
+        
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i];
+            let currentItemID = currentItem[0];
+            let currentItemValue = currentItem[1];
 
-        appendItemToShoppingListEl(currentItem)
+            appendItemToShoppingListEl(currentItem)
+        }
+    }
+    else {
+        shoppingListEl.innerHTML = "No items here... yet!"
     }
 })
 
